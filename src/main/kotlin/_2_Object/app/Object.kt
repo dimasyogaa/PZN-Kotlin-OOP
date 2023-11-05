c4e21f47ee06e9c6bd5c2893c350140b4c612d1f
Yogadimas <yogadimaspambudi@gmail.com>
2023-11-05 23:00:21 +0700

    Object

---
+++ src/main/kotlin/_2_Object/app/Object.kt
@@ -0,0 +1,13 @@
+package _2_Object.app
+
+import _1_Class.data.Person
+
+fun main() {
+    val yoga = Person()
+    val dimas = Person()
+    val pambudi = Person()
+
+    println(yoga)
+    println(dimas)
+    println(pambudi)
+}
