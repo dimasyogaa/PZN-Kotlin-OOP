aa2fffe5ac1484e6ecc898c83f56bf08504e44ed
Yogadimas <yogadimaspambudi@gmail.com>
2023-11-10 23:03:02 +0700

    Data Class

---
+++ src/main/kotlin/_29_Data_Class/app/DataClass.kt
@@ -0,0 +1,33 @@
+package _29_Data_Class.app
+
+import _29_Data_Class.data.Product
+
+/**
+ * Kadang kita sering membuat class yang hanya digunakan sebagai representasi dari data
+ * Data Class cocok digunakan dalam hal ini
+ * Data Class merupakan class yang secara otomatis akan membuatkan function equals, hashCode, toString dan copy dari semua properties yang terdapat di primary constructors yang dimiliki oleh data class
+ *
+ */
+
+fun main() {
+    val product = Product("Indomie", 2500, "Food")
+
+    val product2 = product.copy()
+
+    val product3 = product.copy(name = "Supermi")
+
+    val product4 = Product("Indomie", 2500, "Food")
+
+    println(product)
+    println(product2)
+    println(product3)
+
+    println()
+
+    println(product == product2) // true
+
+    println(product == product3) // false
+
+    println(product == product4) // true
+    println(product.hashCode() == product4.hashCode()) // true
+}
---
+++ src/main/kotlin/_29_Data_Class/data/Product.kt
@@ -0,0 +1,7 @@
+package _29_Data_Class.data
+
+data class Product (
+    val name: String,
+    val price: Int,
+    val category: String
+)
